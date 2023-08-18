0cb58dc64f4efb2fca5936ff4a7a04e32f85468c
JooHyeonKim <subin_282@naver.com>
2023-08-18 20:25:20 +0900

    tabview 만들었다

--- one-grain-daily/ContentView.swift
+++ one-grain-daily/ContentView.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView : View {
+  var body: some View {
+    TabView {
+      Text("The First Tab")
+        .tabItem {
+          Image(systemName: "1.square.fill")
+          Text("First")
         }
-        .padding()
+      Text("Another Tab")
+        .tabItem {
+          Image(systemName: "2.square.fill")
+          Text("Second")
+        }
+      Text("The Last Tab")
+        .tabItem {
+          Image(systemName: "3.square.fill")
+          Text("Third")
+        }
+        .badge(10)
     }
+    .font(.headline)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
