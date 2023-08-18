025f617887aeabd7acb169be3c3f89e6fe2f9963
JooHyeonKim <subin_282@naver.com>
2023-08-18 20:48:05 +0900

    해당 날짜를 저장할 수 있도록 기능 추가(CalanderView.swift)

--- one-grain-daily/CalanderView.swift
+++ one-grain-daily/CalanderView.swift
@@ -7,30 +7,64 @@
 
 import SwiftUI
 
-struct CalendarView: View {
+struct Memo: Identifiable {
+    let id = UUID()
+    let date: Date
+    var text: String
+}
 
+struct CalendarView: View {
     @State private var date = Date()
+    @State private var memoText = ""
+    @State private var memos: [Memo] = []
+    
     @State private var donate = 200
     @State private var have = 29
 
+    var filteredMemos: [Memo] {
+        return memos.filter { Calendar.current.isDate($0.date, inSameDayAs: date) }
+    }
 
     var body: some View {
-        
-        VStack{
+        VStack {
             DatePicker(
                 "Start Date",
                 selection: $date,
                 displayedComponents: [.date]
             )
             .datePickerStyle(.graphical)
-            Spacer().frame(height: 50)
+            
             VStack{
                 Text("기부한 쌀 : \(donate), 보유 쌀: \(have)")
             }
+
+            
+            TextField("메모 추가", text: $memoText)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("메모 저장") {
+                saveMemo()
+            }
+            
+            Text("메모 목록:")
+            List(filteredMemos) { memo in
+                Text("\(memo.date, formatter: dateFormatter): \(memo.text)")
+            }
         }
-        
     }
     
+    private func saveMemo() {
+        memos.append(Memo(date: date, text: memoText))
+        memoText = ""
+    }
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
 }
 
 
