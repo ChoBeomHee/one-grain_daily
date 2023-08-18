50ff73d3a454d164328eee35908814c0aaf7c5ba
JooHyeonKim <subin_282@naver.com>
2023-08-18 20:04:20 +0900

    Initial Commit

---
+++ one-grain-daily/one_grain_dailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_grain_dailyApp.swift
+//  one-grain-daily
+//
+//  Created by 김주현 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct one_grain_dailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
