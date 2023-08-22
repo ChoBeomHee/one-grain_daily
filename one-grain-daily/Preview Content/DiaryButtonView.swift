e2b9ee9f0a7414ea5638e62689ec33ba8083ddd1
JooHyeonKim <subin_282@naver.com>
2023-08-22 12:59:16 +0900

    일기쓰기 버튼을 따로 구현해야 할 것 같음. (DirayButtonView.sift):

---
+++ one-grain-daily/Preview Content/DiaryButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryButtonView.swift
+//  one-grain-daily
+//
+//  Created by 김주현 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct DiaryButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiaryButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryButtonView()
+    }
+}
